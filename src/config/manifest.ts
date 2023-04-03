6042089be1df8160cbe2e3a4bcaeed3f5084ba66
aguscoelho <agustin@conexa.ai>
2023-04-03 20:18:31 -0300

    ci: almost finished vtex controller

--- src/config/manifest.ts
+++ src/config/manifest.ts
@@ -1,18 +1,19 @@
-export default {
-  paymentMethods: [
-    {
-      name: 'Guatapay',
-      allowsSplit: 'disabled',
-    },
-  ],
-  customFields: [
-    {
-      name: 'Client ID - Guatapay',
-      type: 'text',
-    },
-    {
-      name: 'Client Secret - Guatapay',
-      type: 'text',
-    },
-  ],
-};
+import { ICustomField } from 'vtex-package-ts/dist/interfaces';
+
+export const paymentMethods = [
+  {
+    name: 'Guatapay',
+    allowsSplit: 'disabled',
+  },
+];
+
+export const customFields: ICustomField[] = [
+  {
+    name: 'Client ID - Guatapay',
+    type: 'text',
+  },
+  {
+    name: 'Client Secret - Guatapay',
+    type: 'text',
+  },
+];
